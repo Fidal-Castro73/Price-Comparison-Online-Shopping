85b6d82be6955742d40478f2a138266569c28594
BaranikaaSri <baranikaravikumar@gmail.com>
2024-09-22 22:23:39 +0530

    msg

--- cart.js
+++ cart.js
@@ -1,3 +1,15 @@
+document.getElementById('auth-icon').addEventListener('click', function() {
+    const authIcon = document.getElementById('auth-icon');
+    if (authIcon.getAttribute('data-logged-in') === 'true') {
+        // Logout action
+        authIcon.setAttribute('data-logged-in', 'false');
+        authIcon.innerHTML = '<img src="login-icon.png" alt="Login">';
+    } else {
+        // Login action
+        authIcon.setAttribute('data-logged-in', 'true');
+        authIcon.innerHTML = '<img src="user-icon.png" alt="User">';
+    }
+});
 function opentab(tabname) {
     // Get all the tab links and tab contents
     const tablinks = document.querySelectorAll('.tab-links');
